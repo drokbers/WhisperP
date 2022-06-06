4122fb701b54facd172c671b6f1d3a4a6f3241dc
Mehmet Ali <malibayram91@gmail.com>
2022-06-06 21:21:55 +0300

    typo fixed in increase version number script

--- scripts/increase_version_number.dart
+++ scripts/increase_version_number.dart
@@ -5,24 +5,25 @@ Future<void> main() async {
 }
 
 Future<void> increaseVersion() async {
-  final pubcpecFile = File('pubspec.yaml');
-  final pubcpecContent = await pubcpecFile.readAsString();
+  final pubspecFile = File('pubspec.yaml');
+  final pubspecContent = await pubspecFile.readAsString();
 
   final regExp = RegExp(r"version: 0.0.(.*?)\+(.*?)");
 
-  final previousVersionNumberMatch = regExp.firstMatch(pubcpecContent);
+  final previousVersionNumberMatch = regExp.firstMatch(pubspecContent);
   if (previousVersionNumberMatch == null) return;
 
   final previousVersion =
       int.tryParse("${previousVersionNumberMatch.group(1)}");
 
   if (previousVersion != null) {
-    final finalPubcpecContent = pubcpecContent.replaceAll(
+    final finalpubspecContent = pubspecContent.replaceAll(
       'version: 0.0.$previousVersion+$previousVersion',
       'version: 0.0.${previousVersion + 1}+${previousVersion + 1}',
     );
 
-    await pubcpecFile.writeAsString(finalPubcpecContent);
-  } else
+    await pubspecFile.writeAsString(finalpubspecContent);
+  } else {
     return;
+  }
 }
