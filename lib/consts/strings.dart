35deff61ea8e4454068445dba107a7be3e74334a
“Serdar <aksoyserdar7@gmail.com>
2022-06-07 23:39:08 +0300

    welcome screen updated

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,6 +1,9 @@
 class Str {
   static const profilePageTitle = "Profile Page";
   static const messagesPageTitle = "Mesajlar";
+  static const welcomeMessage1 = "Welcome to WhisperP";
+  static const welcomeMessage2 =
+      "Talk freely and safely to \nwhomever you want.";
   static const dummyProfilePhotoUrl =
       "https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png";
 }
--- lib/ui/screens/welcome/welcome_screen.dart
+++ lib/ui/screens/welcome/welcome_screen.dart
@@ -10,68 +10,83 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            const Spacer(flex: 2),
-            Image.asset(AssetsPath.welcomeImage),
-            const Spacer(flex: 3),
-            Text(
-              "Welcome to our freedom \nmessaging app",
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .headline5!
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-            const Spacer(),
-            Text(
-              "Freedom talk any person of your \nmother language.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Theme.of(context)
-                    .textTheme
-                    .bodyText1!
-                    .color!
-                    .withOpacity(0.64),
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(flex: 2),
+              Expanded(
+                flex: 8,
+                child: Image.asset(
+                  AssetsPath.welcomeImage,
+                ),
               ),
-            ),
-            const Spacer(flex: 3),
-            FittedBox(
-              child: TextButton(
-                  onPressed: () {
-                    Hive.box(BoxNames.settings).put('is-skipped', true);
-
-                    Navigator.pushNamed(
-                      context,
-                      RouteNames.signInOrSignUpScreen,
-                    );
-                  },
-                  child: Row(
-                    children: [
-                      Text(
-                        "Skip",
-                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1!
-                                  .color!
-                                  .withOpacity(0.8),
-                            ),
+              const Spacer(flex: 3),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  Str.welcomeMessage1,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline5!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
                       ),
-                      const SizedBox(width: kDefaultPadding / 4),
-                      Icon(
-                        Icons.arrow_forward_ios,
-                        size: 16,
-                        color: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .color!
-                            .withOpacity(0.8),
-                      )
-                    ],
-                  )),
-            )
-          ],
+                ),
+              ),
+              const Spacer(),
+              Expanded(
+                flex: 2,
+                child: Text(
+                  Str.welcomeMessage2,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .color!
+                          .withOpacity(0.7)),
+                ),
+              ),
+              const Spacer(flex: 2),
+              FittedBox(
+                child: TextButton(
+                    onPressed: () {
+                      Hive.box(BoxNames.settings).put('is-skipped', true);
+                      Navigator.pushNamed(
+                        context,
+                        RouteNames.signInOrSignUpScreen,
+                      );
+                    },
+                    child: Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Text(
+                            "Skip",
+                            style:
+                                Theme.of(context).textTheme.bodyText1!.copyWith(
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .bodyText1!
+                                          .color!
+                                          .withOpacity(0.8),
+                                    ),
+                          ),
+                          const SizedBox(width: kDefaultPadding / 4),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            size: 16,
+                            color: Theme.of(context)
+                                .textTheme
+                                .bodyText1!
+                                .color!
+                                .withOpacity(0.8),
+                          )
+                        ],
+                      ),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
