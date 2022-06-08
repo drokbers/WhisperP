234a8bc8f1ff26dbb1093d4512346ee2af5792d4
“Serdar <aksoyserdar7@gmail.com>
2022-06-08 21:11:43 +0300

    ParentDataWidget bug solved.

--- lib/ui/screens/welcome/welcome_screen.dart
+++ lib/ui/screens/welcome/welcome_screen.dart
@@ -9,8 +9,8 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
+      body: Center(
+        child: SafeArea(
           child: Column(
             children: [
               const Spacer(flex: 2),
@@ -21,69 +21,60 @@ class WelcomeScreen extends StatelessWidget {
                 ),
               ),
               const Spacer(flex: 3),
-              Expanded(
-                flex: 2,
-                child: Text(
-                  Str.welcomeMessage1,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline5!.copyWith(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                      ),
-                ),
+              Text(
+                Str.welcomeMessage1,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline5!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
               ),
               const Spacer(),
-              Expanded(
-                flex: 2,
-                child: Text(
-                  Str.welcomeMessage2,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Theme.of(context)
-                          .textTheme
-                          .bodyText1!
-                          .color!
-                          .withOpacity(0.7)),
-                ),
+              Text(
+                Str.welcomeMessage2,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .color!
+                        .withOpacity(0.7)),
               ),
               const Spacer(flex: 2),
               FittedBox(
                 child: TextButton(
-                    onPressed: () {
-                      Hive.box(BoxNames.settings).put('is-skipped', true);
-                      Navigator.pushNamed(
-                        context,
-                        RouteNames.signInOrSignUpScreen,
-                      );
-                    },
-                    child: Expanded(
-                      flex: 1,
-                      child: Row(
-                        children: [
-                          Text(
-                            "Skip",
-                            style:
-                                Theme.of(context).textTheme.bodyText1!.copyWith(
-                                      color: Theme.of(context)
-                                          .textTheme
-                                          .bodyText1!
-                                          .color!
-                                          .withOpacity(0.8),
-                                    ),
-                          ),
-                          const SizedBox(width: kDefaultPadding / 4),
-                          Icon(
-                            Icons.arrow_forward_ios,
-                            size: 16,
-                            color: Theme.of(context)
-                                .textTheme
-                                .bodyText1!
-                                .color!
-                                .withOpacity(0.8),
-                          )
-                        ],
+                  onPressed: () {
+                    Hive.box(BoxNames.settings).put('is-skipped', true);
+                    Navigator.pushNamed(
+                      context,
+                      RouteNames.signInOrSignUpScreen,
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      Text(
+                        "Skip",
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .color!
+                                  .withOpacity(0.8),
+                            ),
                       ),
-                    )),
+                      const SizedBox(width: kDefaultPadding / 4),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 16,
+                        color: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .color!
+                            .withOpacity(0.8),
+                      )
+                    ],
+                  ),
+                ),
               )
             ],
           ),
